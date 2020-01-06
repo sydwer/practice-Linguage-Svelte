6af6540b118534e7c5d7d7f98b52f5344faf2fcb
Sydney Werbach <sydwer@gmail.com>
2020-01-06 16:57:27 -0700

    started on Router, to test out Svelte, added in materialize css as well

---
+++ src/layout/Navbar.svelte
@@ -0,0 +1,16 @@
+  <script>
+    import { Link } from 'svelte-routing';
+  </script>
+  
+  <nav>
+    <div class="nav-wrapper">
+     <div class = "container">
+        <Link to="/"><span class="brand-logo">Linguage</span></Link>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><Link to="/">Explore</Link></li>
+        <li><Link to="/">Compare</Link></li>
+        <li><Link to="/">Create</Link></li>
+      </ul>
+     </div>
+    </div>
+  </nav>
